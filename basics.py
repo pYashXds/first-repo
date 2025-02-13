84452b6051a8d8bfb68858d9bca15e5b7b0ed90d
pYashXds <pyash.xds@gmail.com>
2025-02-13 16:42:51 +0530

    add datatype cocepts

--- basics.py
+++ basics.py
@@ -3,3 +3,10 @@
 a = "Hello World"
 
 print(a)
+
+b = {1,2,3,4}
+c = [1,2,3,4]
+d = (1,2,3,4)
+e = {'one':1, 'two':2, 'three':3, 'four':4}
+
+print(type(a), " ", type(b), " ", type(c), " ", type(d), " ", type(e))
